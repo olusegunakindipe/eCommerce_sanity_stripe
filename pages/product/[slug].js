43c71a8642476053b910dcc5c45355ff2eaa5792
Francis <Akinfergie@gmail.com>
2022-07-04 13:32:07 +0100

    Initial Commit

---
+++ pages/product/[slug].js
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from 'react';
+import { client, urlFor } from '../../lib/client';
+import {
+  AiFillStar,
+  AiOutlineStar,
+  AiOutlineMinus,
+  AiOutlinePlus,
+} from 'react-icons/ai';
+import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
+const ProductDetails = ({ product, products }) => {
+  const [index, setIndex] = useState(0);
+  const { image, name, details, price } = product;
+  const { decQty, incQty, qty, onAdd, setShowCart, cartItems } =
+    useStateContext();
+
+  const handleBuyNow = () => {
+    setShowCart(true);
+  };
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div>
+          <div className="image-container">
+            <img src={urlFor(image[index])} className="product-detail-image" />
+          </div>
+          <div className="small-images-container">
+            {image.map((item, i) => (
+              <img
+                key={item._key}
+                src={urlFor(item)}
+                className={
+                  i === index ? 'small-image selected-image' : 'small-image'
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="product-detail-desc">
+          <h2>{name}</h2>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>{20}</p>
+          </div>
+          <h4>Details:</h4>
+          <p>{details}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity: </h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick={decQty}>
+                <AiOutlineMinus />
+              </span>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}>
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
+              {' '}
+              Add to Cart
+            </button>
+            <button
+              type="button"
+              className="buy-now"
+              onClick={() => handleBuyNow()}
+            >
+              {' '}
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
+
+export const getStaticProps = async ({ params: { slug } }) => {
+  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+  const product = await client.fetch(query);
+  const productQuery = `*[_type == "product"]`;
+  const products = await client.fetch(productQuery);
+
+  return {
+    props: {
+      product,
+      products,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const query = `*[_type == "product"] {
+        slug {
+            current
+        }
+    }`;
+
+  const products = await client.fetch(query);
+  const paths = products.map((product) => ({
+    params: {
+      slug: product.slug.current,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+};
---
+++ studio/schemas/banner.js
@@ -0,0 +1,60 @@
+export default {
+  name: 'banner',
+  title: 'Banner',
+  type: 'document',
+  fields: [
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'buttonText',
+      title: 'ButtonText',
+      type: 'string',
+    },
+    {
+      name: 'product',
+      type: 'string',
+      title: 'Product',
+    },
+    {
+      name: 'desc',
+      type: 'string',
+      title: 'Desc',
+    },
+    {
+      name: 'smallText',
+      title: 'SmallText',
+      type: 'string',
+    },
+    {
+      name: 'midText',
+      title: 'MidText',
+      type: 'string',
+    },
+    {
+      name: 'largeText1',
+      title: 'LargeText1',
+      type: 'string',
+    },
+    {
+      name: 'largeText2',
+      title: 'LargeText2',
+      type: 'string',
+    },
+    {
+      name: 'discount',
+      title: 'Discount',
+      type: 'string',
+    },
+    {
+      name: 'salestime',
+      title: 'SaleTime',
+      type: 'string',
+    },
+  ],
+};

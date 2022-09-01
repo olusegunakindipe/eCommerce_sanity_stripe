876485db07994044c462fe509cbe3ddd9f825b0e
Francis <Akinfergie@gmail.com>
2022-09-01 18:40:30 +0100

    resolve eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  ignorePatterns: ['node_modules', 'dist'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  globals: {
+    workbox: true,
+    JSX: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 9,
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+};
